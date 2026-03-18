440c660cb2953054052b40a585e8182c698ec09a
Alice <alice.gruby@gmail.com>
2026-03-18 18:09:33 +0100

    Modif LoginSuccessHandler

--- src/Security/LoginSuccessHandler.php
+++ src/Security/LoginSuccessHandler.php
@@ -6,6 +6,8 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
 {
@@ -14,18 +16,26 @@ class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
     public function __construct(UrlGeneratorInterface $urlGenerator)
     {
         $this->urlGenerator = $urlGenerator;
+        $this->jwtManager = $jwtManager;
     }
 
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token): RedirectResponse
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token)
     {
         $user = $token->getUser();
 
+        // Si c'est une requête API
+        if (str_starts_with($request->getPathInfo(), '/api')) {
+            $jwt = $this->jwtManager->create($user);
+            return new JsonResponse(['token' => $jwt]);
+        }
+
+        // Sinon redirection normale
         if (in_array('ROLE_ADMIN', $user->getRoles(), true)) {
             return new RedirectResponse($this->urlGenerator->generate('admin_dashboard'));
         }
 
         $targetPath = $request->request->get('_target_path')
-        ?? $request->getSession()->get('_security.main.target_path');
+            ?? $request->getSession()->get('_security.main.target_path');
 
         if ($targetPath) {
             return new RedirectResponse($targetPath);
