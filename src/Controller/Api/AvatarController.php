3c50c70e4969d971c67ade832171466ae6cc1975
Alice <alice.gruby@gmail.com>
2026-03-18 12:39:57 +0100

    Add AvatarController API Flutter

---
+++ src/Controller/Api/AvatarController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Repository\AvatarRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AvatarController extends AbstractController
+{
+    #[Route('/api/avatars', name: 'api_avatars', methods: ['GET'])]
+    public function index(AvatarRepository $avatarRepository): JsonResponse
+    {
+        $avatars = $avatarRepository->findBy([], ['id' => 'ASC']);
+
+        $data = array_map(fn($a) => [
+            'id'        => $a->getId(),
+            'image_url' => $a->getImageUrl(),
+        ], $avatars);
+
+        return $this->json($data);
+    }
+}
